32bf1bc0674e553cf9f1226abf044dc46506662f
Dmirty <ddigorev@gmail.com>
2022-07-23 19:26:23 +0300

    a

--- Semenar4/task29/Program.cs
+++ Semenar4/task29/Program.cs
@@ -3,24 +3,31 @@
 //1, 2, 5, 7, 19 -> [2, 4, 25, 49, 361]
 //6, 1, 33 -> [36, 1, 1089]
 
-namespace task29 {
-    class Program {
-        static void Main() {
+namespace task29
+{
+    class Program
+    {
+        static void Main()
+        {
             int size = new Random().Next(5, 11);                      // Задание псевдослучайной длины массива:
             int[] array = new int[size];                              // Создание массива псевдослучайной длины:
 
             FillArray(array);
             PrintArray(array);
         }
-        static void PrintArray(int[] Array) {                         // Функция печати массива квадратов исходного:
-            for (int i = 0; i < Array.GetLength(0); i++) {
+        static void PrintArray(int[] Array)
+        {                         // Функция печати массива квадратов исходного:
+            for (int i = 0; i < Array.GetLength(0); i++)
+            {
                 Console.Write($"{Array[i] * Array[i]} ");
             }
         }
 
-        static void FillArray(int[] Array) {                          // Функция заполнения массива:
-            for (int i = 0; i < Array.GetLength(0); i++) {
-                Array[i] = new Random().Next(1,41);
+        static void FillArray(int[] Array)
+        {                          // Функция заполнения массива:
+            for (int i = 0; i < Array.GetLength(0); i++)
+            {
+                Array[i] = new Random().Next(1, 41);
             }
         }
     }
