6a10667de225c9971f454ab57e876a6e40ad066d
Dmirty <ddigorev@gmail.com>
2022-08-08 17:15:59 +0300

    Semenar 9 added

---
+++ Semenar9/Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+namespace Task66
+{
+    class Program
+    {
+        public static void Main()
+        {
+            const int M = 1, N = 100;
+            int summ = 0;
+
+            for (int i = M; i < N; i++)
+            {
+                summ += i;
+            }
+
+            Console.WriteLine(summ);
+        }
+    }
+}
---
+++ Semenar9/Task68/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 29
+
+
+namespace Task68
+{
+    class Program
+    {
+        public static void Main()
+        {
+            const int N = 2, M = 3;
+
+            Console.WriteLine(AkkermanFanction(M, N));
+        }
+
+        public static int AkkermanFanction(int m, int n)
+        {
+            if (m < 0 || n < 0) return 0;
+            if (m == 0) return n + 1;
+            if (n == 0 && m > 0) return AkkermanFanction(m - 1, 1);
+            return AkkermanFanction(m - 1, AkkermanFanction(m, n - 1));
+        }
+    }
+}
